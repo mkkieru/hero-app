5c754dd051ca7330e5b643f8c964b4ab8396854a
mkkieru <mkieru55@gmail.com>
2021-08-07 21:55:10 +0300

    Add hero class

--- src/main/java/model/hero.java
+++ src/main/java/model/hero.java
@@ -1,4 +1,42 @@
 package model;
 
+import java.security.PrivateKey;
+import java.util.ArrayList;
+import java.util.List;
+
 public class hero {
+
+    private String name;
+    private Integer age;
+    private String specialPower;
+    private String weakness;
+    private static List<hero> instances = new ArrayList<>();
+
+    public hero(String name, Integer age, String specialPower, String weakness){
+        this.name = name;
+        this.specialPower = specialPower;
+        this.weakness = weakness;
+        this.age = age;
+        instances.add(this);
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecialPower() {
+        return specialPower;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public static List<hero> getInstances() {
+        return instances;
+    }
 }
