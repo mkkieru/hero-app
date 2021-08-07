85ac5262171a8a54c24d1517ced5b784f57b2dfa
mkkieru <mkieru55@gmail.com>
2021-08-07 21:54:40 +0300

    Add an alert to the main class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,12 @@
+import static spark.Spark.*;
+import static javax.swing.JOptionPane.showMessageDialog;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+
+        showMessageDialog(null,"This is an alert!!");
+
+    }
 }
