cd58e6864dfecc3a2be77766e2d5736eb1f57132
mkkieru <mkieru55@gmail.com>
2021-08-10 01:08:07 +0300

    Add fully working code

--- src/main/java/model/SQUAD.java
+++ src/main/java/model/SQUAD.java
@@ -27,6 +27,7 @@ public class SQUAD {
             if (instances.get(i).squadName.equals(squadName)) {//squad1
                 check = true;
                 instance = i;
+            }
 
                 for (int x = 0; x < instances.get(i).heros.size(); x++) {
 
@@ -34,13 +35,11 @@ public class SQUAD {
 
                         showMessageDialog(null, "This Hero is in another squad");
                         checkHero = true;
-                        break;
                     }
                 }
-                break;
-            }
         }
-        if (!check){
+
+        if (!check && !checkHero){
             instances.add(this);
             instances.get(instances.size()-1).heros.add(hero);
             showMessageDialog(null, "The Squad has been created with the new hero");
